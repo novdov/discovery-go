65145d88130e0e1a0a7166439aea80481e292639
novdov <rpwjd56@gmail.com>
2019-12-17 20:13:09 +0900

    ch03) Add example on strings/bytes

--- ch03/hangul/hangul_test.go
+++ ch03/hangul/hangul_test.go
@@ -12,3 +12,32 @@ func ExampleHasConsonantSuffix() {
 	// true
 	// false
 }
+
+func Example_printBytes() {
+	s := "가나다"
+	for i := 0; i < len(s); i ++ {
+		fmt.Printf("%x:", s[i])
+	}
+	fmt.Println()
+	// Output:
+	// ea:b0:80:eb:82:98:eb:8b:a4:
+}
+
+func Example_modifyBytes() {
+	b := []byte("가나다")
+	b[2]++
+	fmt.Println(string(b))
+	// Output:
+	// 각나다
+}
+
+func Example_strCat() {
+	s := "abc"
+	ps := &s
+	s += "def"
+	fmt.Println(s)
+	fmt.Println(*ps)
+	// Output:
+	// abcdef
+	// abcdef
+}
